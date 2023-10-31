c559f8c98dc4caf16861e9cebfacfa6e256ec4ad
Jason Warren <74985192+JasonWarrenUK@users.noreply.github.com>
2023-10-31 12:08:38 +0000

    Tidied up index.html Deferred js Updated addresses to make sure links work Added basic semantics

--- docs/pseudo.js
+++ docs/pseudo.js
@@ -4,46 +4,43 @@ let taskList = [];
 
 
 //Adding Tasks
+  const taskBox = document.getElementById("newTaskForm");
 
-const taskBox = document.getElementById("newTaskForm");
+  const taskAdd = taskBox.getElement("button");
 
-const taskAdd = taskBox.getElement("button");
-
-taskAdd.addEventListener(click) {
-  addTask(stuff);
-}
-
-function addTask(taskName, taskDue, taskDo, taskStatus, taskPriority) {
-  let task = {
-    name: taskName,
-    due: taskDue,
-    do: taskDo,
-    status: taskStatus,
-    priority: taskPriority
+  taskAdd.addEventListener(click) {
+    addTask(stuff);
   }
-  
-  taskList.push(task);
-}
+
+  function addTask(taskName, taskDue, taskDo, taskStatus, taskPriority) {
+    let task = {
+      name: taskName,
+      due: taskDue,
+      do: taskDo,
+      status: taskStatus,
+      priority: taskPriority
+    }
+    taskList.push(task);
+  }
 
 
 //Filters & Views
+  const listViews = document.getElementById("listViews");
 
-const listViews = document.getElementById("listViews");
+  const listAll = listViews.getElementById("listAll");
+  const listOpen = listViews.getElementById("listOpen");
+  const listUrgent = listViews.getElementById("listUrgent");
 
-const listAll = listViews.getElementById("listAll");
-const listOpen = listViews.getElementById("listOpen");
-const listUrgent = listViews.getElementById("listUrgent");
+  listAll.addEventListener(click) {
+    view(stuff);
+  }
+  listOpen.addEventListener(click) {
+    view(stuff);
+  }
+  listUrgent.addEventListener(click) {
+    view(stuff);
+  }
 
-listAll.addEventListener(click) {
-  view(stuff);
-}
-listOpen.addEventListener(click) {
-  view(stuff);
-}
-listUrgent.addEventListener(click) {
-  view(stuff);
-}
-
-function view(stuff) {
-  return taskList.filter(stuff)
-}
+  function view(stuff) {
+    return taskList.filter(stuff)
+  }
