40443c76295e08c4bfa0e451d8fd9f304df9bbb5
Jason Warren <contact.jwarren@gmail.com>
2023-11-09 20:36:40 +0000

    Test works Added cleaner console groups Closes Test: Task Delete #41

--- js/test.js
+++ js/test.js
@@ -13,6 +13,131 @@ test("Tasks from array show on screen", () => {
   equal(taskImportanceElement.textContent, 'high', 'Task importance is correct');
 });
 
+test("Clicking on complete button marks the task as complete", () => {
+  const mockTask = new Task("Complete Test", "2023-11-09", "normal");
+
+  console.groupCollapsed("Set Up");
+    console.groupCollapsed("Before Pushing to Tasks");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+    console.groupEnd();
+    
+    tasks.push(mockTask);
+
+    console.groupCollapsed("After Pushing to Tasks");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+    console.groupEnd();
+  console.groupEnd();
+  
+  console.group("Tests");
+    notEqual(mockTask.status, "complete", "Task should not be marked as complete");
+
+    displayListItems(tasks);
+
+    const mockLi = document.getElementById(mockTask.id);
+    console.log("Task Li found: " + mockLi);
+    completeTaskJS(mockLi);
+
+    console.groupCollapsed("After Completing Task");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+    console.groupEnd();
+
+    equal(mockTask.status, "complete", "Task should be marked as complete");
+  console.groupEnd();
+
+  console.groupCollapsed("Clean Up")
+    console.groupCollapsed("Before CleanUp");
+      console.log("Tasks in List: " + tasks.length);
+      console.log("Tasks in Local Storage: " + localStorage.getItem('tasks'));
+    console.groupEnd();
+    
+    tasks.splice(tasks.findIndex(task => task.name === mockTask.name), 1);
+
+    console.groupCollapsed("Array Cleaned");
+      console.log("Tasks in List: " + tasks.length);
+      console.log("Tasks in Local Storage: " + localStorage.getItem('tasks'));
+    console.groupEnd();
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    console.groupCollapsed("Local Storage Cleaned");
+      console.log("Tasks in List: " + tasks.length);
+      console.log("Tasks in Local Storage: " + localStorage.getItem('tasks'));
+    console.groupEnd();
+  console.groupEnd();
+});
+
+test("Deleting an entry removes it from the list", () => {
+  const mockTask = new Task("Delete Test", "2023-11-09", "normal");
+  let mockTaskExists = doesXHaveY(tasks, mockTask);
+
+  console.groupCollapsed("Set Up");
+    console.groupCollapsed("Before Pushing to Tasks");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+      console.log("List Contains Mock Task: " + mockTaskExists);
+    console.groupEnd();
+    
+    tasks.push(mockTask);
+    mockTaskExists = doesXHaveY(tasks, mockTask);
+
+    console.groupCollapsed("After Pushing to Tasks");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+      console.log("List Contains Mock Task: " + mockTaskExists);
+    console.groupEnd();
+  console.groupEnd();
+
+  console.group("Tests");
+    equal(mockTaskExists, true, "Task Should Exist");
+
+    displayListItems(tasks);
+
+    const mockLi = document.getElementById(mockTask.id);
+    console.log("Task Li found: " + mockLi);
+    deleteTaskJS(mockLi);
+    mockTaskExists = doesXHaveY(tasks, mockTask);
+
+    console.groupCollapsed("After Deleting Task");
+      console.log("Name: " + mockTask.name);
+      console.log("ID: " + mockTask.id);
+      console.log("Status: " + mockTask.status);
+      console.log("Tasks in List: " + tasks.length);
+      console.log("List Contains Mock Task: " + mockTaskExists);
+    console.groupEnd();
+
+    notEqual(mockTaskExists, true, "Task Should Not Exist");
+  console.groupEnd();
+
+  console.groupCollapsed("Clean Up")
+    console.groupCollapsed("Before Cleanup");
+      console.log("Tasks in List: " + tasks.length);
+      console.log("Tasks in Local Storage: " + localStorage.getItem('tasks'));
+    console.groupEnd();
+    
+    tasks.splice(tasks.findIndex(task => task.name === mockTask.name), 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    console.groupCollapsed("After Cleanup");
+      console.log("Tasks in List: " + tasks.length);
+      console.log("Tasks in Local Storage: " + localStorage.getItem('tasks'));
+    console.groupEnd();
+  console.groupEnd();
+});
+
+
 test("Toggling the filter hides completed tasks from the list", () => {
   const completeButton = document.getElementById("completeButton");
   const actualContainer = document.getElementById('to-do-list');
@@ -25,52 +150,7 @@ test("Toggling the filter hides completed tasks from the list", () => {
   equal(actualContainer.innerHTML, expected)
 });
 
-
-//! Work In Progress
-
-test("Clicking on complete button marks the task as complete", () => {
-  const mockTask = new Task("Mock Task", "2023-11-09", "normal");
-  console.groupCollapsed("Before Pushing to Tasks");
-    console.log("Name: " + mockTask.name);
-    console.log("ID: " + mockTask.id);
-    console.log("Status: " + mockTask.status);
-    console.log("Tasks in List: " + tasks.length);
-  console.groupEnd();
-  
-  tasks.push(mockTask);
-
-  console.groupCollapsed("After Pushing to Tasks");
-    console.log("Name: " + mockTask.name);
-    console.log("ID: " + mockTask.id);
-    console.log("Status: " + mockTask.status);
-    console.log("Tasks in List: " + tasks.length);
-  console.groupEnd();
-  
-  notEqual(mockTask.status, "complete", "Task should not be marked as complete");
-
-  displayListItems(tasks);
-
-  const mockLi = document.getElementById(mockTask.id);
-  console.log("Task Li found: " + mockLi);
-  completeTaskJS(mockLi);
-
-  console.groupCollapsed("After Completing Task");
-    console.log("Name: " + mockTask.name);
-    console.log("ID: " + mockTask.id);
-    console.log("Status: " + mockTask.status);
-    console.log("Tasks in List: " + tasks.length);
-  console.groupEnd();
-
-  equal(mockTask.status, "complete", "Task should be marked as complete");
-
-  console.log("Tasks in List Before Deleting: " + tasks.length);
-  tasks.splice(tasks.findIndex(task => task.name === mockTask.name), 1);
-  console.log("Tasks in List After Deleting: " + tasks.length);
-});
-
-
 //TODO Write These
-
 test("Submitting a new task adds it to the list", () => {
   //could I test this park of the function
   //const newTask = new Task(taskName, taskDateDue, taskImportance);
@@ -80,8 +160,4 @@ test("Submitting a new task adds it to the list", () => {
   //tasks.push(newTask);
   //set the stored list
   //localStorage.setItem('tasks', JSON.stringify(tasks));
-});
-
-test("Deleting an entry removes it from the list", () => {
-  // test goes here
 });
